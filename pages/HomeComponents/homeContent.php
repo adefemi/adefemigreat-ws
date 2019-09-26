5e710f89ae721542f9f3817ee260570e6e0b621a
adefemi <adefemigreat1995@gmail.com>
2019-09-26 11:25:04 +0100

    updated home content

--- pages/HomeComponents/homeContent.php
+++ pages/HomeComponents/homeContent.php
@@ -1,6 +1,127 @@
 <div>
-    <img src="https://cdn.pixabay.com/photo/2019/08/29/06/24/elephants-4438284_960_720.jpg" alt="">
-    <img src="https://cdn.pixabay.com/photo/2019/09/11/13/47/grass-4468766_960_720.jpg" alt="">
-    <img src="https://cdn.pixabay.com/photo/2019/09/13/07/41/sunrise-4473359__340.jpg" alt="">
-    <img src="https://cdn.pixabay.com/photo/2019/09/12/08/56/pumpkin-4471000__340.jpg" alt="">
+    <div class="container-section">
+        <div class="section-1 home-heading">
+            <h1 class="title">Hello! I'm Adefemi</h1>
+            <h3 class="title2">Am a Software Developer!!!</h3>
+        </div>
+        <div class="section-2 home-heading-image">
+            <div class="img-con-cover">
+                <div class="img-con" style="background-image: url('https://cdn.pixabay.com/photo/2019/09/23/19/47/sheep-4499562_1280.jpg')"></div>
+            </div>
+        </div>
+    </div>
+    <div class="container-section">
+        <div class="section-1 home-heading section-border">
+            <p class="context">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi atque consequatur cumque debitis dignissimos dolore doloribus,
+                ducimus eius eos eveniet expedita facilis fuga ipsam iusto nam neque nobis, pariatur perferendis placeat quas recusandae reiciendis
+                rem sed similique sint voluptatem. Aspernatur corporis eligendi fugit illo nam necessitatibus, possimus recusandae voluptatem?
+            </p>
+        </div>
+        <div class="section-2 home-heading">
+            <h1 class="title text-center margin-top-30">About me</h1>
+        </div>
+    </div>
+    <div class="container-section margin-top-50">
+        <div class="section-1 home-heading">
+            <h1 class="title text-center margin-top-30">What i can do</h1>
+        </div>
+        <div class="section-2 home-skill-con section-border">
+            <div class="skill-con">
+                <div class="skill-card">Django</div>
+                <div class="skill-card">JavaScript</div>
+                <div class="skill-card">React Js</div>
+                <div class="skill-card">HTML</div>
+                <div class="skill-card">SCSS</div>
+                <div class="skill-card">CSS</div>
+                <div class="skill-card">PHP</div>
+                <div class="skill-card">Flutter</div>
+                <div class="skill-card">Aws</div>
+                <div class="skill-card">Godot</div>
+                <div class="skill-card">Unity</div>
+                <div class="skill-card">Python</div>
+                <div class="skill-card">C#</div>
+                <div class="skill-card">PhotoShop</div>
+                <div class="skill-card">Illustrator</div>
+                <div class="skill-card">Drawing</div>
+            </div>
+        </div>
+    </div>
+
+    <div class="margin-top-50 home-segment">
+        <div class="title3 text-center">Some of my projects</div>
+    </div>
+
+    <div class="container-section">
+        <div class="section-2 home-heading-image">
+            <div class="img-con-cover">
+                <div class="img-con" style="background-image: url('https://cdn.pixabay.com/photo/2019/09/23/20/55/castle-4499711_1280.jpg')"></div>
+            </div>
+        </div>
+        <div class="section-1 home-heading">
+            <h1 class="project-title">Project1</h1>
+            <p class="context margin-right-10">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Animi autem cupiditate eveniet illum incidunt ipsum, saepe tenetur voluptatibus. Autem, sapiente?
+            </p>
+            <button class="button-main">
+                Explore
+            </button>
+        </div>
+    </div>
+
+    <div class="container-section">
+        <div class="section-1 home-heading">
+            <h1 class="project-title">Project2</h1>
+            <p class="context margin-right-10">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Animi autem cupiditate eveniet illum incidunt ipsum, saepe tenetur voluptatibus. Autem, sapiente?
+            </p>
+            <button class="button-main">
+                Explore
+            </button>
+        </div>
+        <div class="section-2 home-heading-image">
+            <div class="img-con-cover">
+                <div class="img-con" style="background-image: url('https://cdn.pixabay.com/photo/2019/09/13/16/34/landscape-4474344_1280.jpg')"></div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container-section">
+        <div class="section-1 home-skill-con section-border">
+            <div style="width: 100%">
+                <div class="form-group">
+                    <label>
+                        <span class="label">NAME</span>
+                        <input type="text" name="a-username" autocomplete="off"
+                               readonly onfocus="this.removeAttribute('readonly');"/>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <span class="label">EMAIL</span>
+                        <input name="a-password" type="text" autocomplete="off"
+                               readonly onfocus="this.removeAttribute('readonly');"/>
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label>
+                        <span class="label">MESSAGE</span>
+                        <textarea rows="5" name="a-password"  autocomplete="off"
+                                  readonly onfocus="this.removeAttribute('readonly');"></textarea>
+                    </label>
+                </div>
+                <br>
+                <button class="button-main">Login</button>
+            </div>
+        </div>
+        <div class="section-2 home-heading">
+            <h1 class="title text-center margin-top-30">Contact me</h1>
+        </div>
+    </div>
+    <div class="margin-top-50 footer">
+        Adefemigreat &copy; 2018
+    </div>
 </div>
